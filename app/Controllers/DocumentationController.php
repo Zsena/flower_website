7496e8656a0971a82b279152cf2443e6be9d126b
zsena <zsanett.tamas87@gmail.com>
2021-12-13 12:51:59 +0100

    Initial commit

---
+++ app/Controllers/DocumentationController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Repositories\ProductRepository as Repository;
+use Core\Exceptions\TemplateNotFoundException;
+
+class DocumentationController extends Controller {
+
+    private $repository;
+
+    public function __construct($loader, $twig) {
+        parent::__construct($loader, $twig);
+        $this->repository = new Repository();
+    }
+
+    public function render() {
+
+        $product = $this->repository->getProductById(1);
+
+        $data = [
+            'product' => $product
+        ];
+
+        $templateName = "@templates/documentation.twig";
+
+        try {
+            $tpl = $this->twig->load($templateName);
+            echo $tpl->render($data);
+        } catch(TemplateNotFoundException $e) {
+            throw new TemplateNotFoundException($templateName);
+        }
+
+        
+    }
+}
---
+++ routes/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+$router->post('/hello/{id}', 'TestController@test');
+
+/*
+$router->get('/hello/{name}', function($params) {
+    echo "hello ". $params['name'];
+});
+*/
+
+$router->get('/p/{slug:string}/{id:int}', 'TestController@testMethod');
+
+$router->get('/', 'StartPageController@render');
+
+$router->get('/test/{slug}/{id}', 'TestController@testTwo');
+
+$router->get('/dokumentacio', 'DocumentationController@render');
+
+$router->get('/blog/{id}', 'StartPageController@blog');
+
+$router->post('/bar', function() { 
+    echo "POST bar\n"; 
+});
+
+$router->get('/cms', 'ContentController@cms');
+
+$router->post('/api/save', 'ContentController@save');
+
+$router->post('/parsed-body-test', 'TestController@parsedBody');
+
+
