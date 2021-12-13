d75d00db96d90d170c059e3dd05530283ed9a70d
Zsanett Tamás <zsanett.tamas87@gmail.com>
2021-12-13 18:22:52 +0100

    uikit npm config, js notification config, layout fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log('mükszik');
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification("<span uk-icon='icon: check'></span> Kérj ajánlatot itt: <a class='uk-link-text' href='/kapcsolat'>Árajánlat</a>", { status:'success',  pos: 'bottom-right' });
